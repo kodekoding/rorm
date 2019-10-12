55ccdc5d131f72008a1b53d4e371cb510ee66805
Raditya Pratama <radityaapratamaa@gmail.com>
2019-10-12 23:21:57 +0700

    list of function interface

--- interface.go
+++ interface.go
@@ -1,1 +1,12 @@
 package rorm
+
+import rormEngine "github.com/radityaapratamaa/rorm/engine"
+
+type EngineInterface interface {
+	Column(col ...string) *rormEngine.RormEngine
+	Where(col, value string, opt ...string) *rormEngine.RormEngine
+	Or(col, value string, opt ...string) *rormEngine.RormEngine
+	OrderBy(col, value string) *rormEngine.RormEngine
+	Limit(limit int, offset ...int) *rormEngine.RormEngine
+	Get(tableName string) error
+}

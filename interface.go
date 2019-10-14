daaf6d477930548ec237b5a315e505ad84199d9e
Raditya Pratama <radityaapratamaa@gmail.com>
2019-10-14 20:14:38 +0700

    register new function

--- interface.go
+++ interface.go
@@ -1,12 +1,32 @@
 package rorm
 
-import rormEngine "github.com/radityaapratamaa/rorm/engine"
+// import rormEngine "github.com/radityaapratamaa/rorm/engine"
 
 type EngineInterface interface {
-	Column(col ...string) *rormEngine.RormEngine
-	Where(col, value string, opt ...string) *rormEngine.RormEngine
-	Or(col, value string, opt ...string) *rormEngine.RormEngine
-	OrderBy(col, value string) *rormEngine.RormEngine
-	Limit(limit int, offset ...int) *rormEngine.RormEngine
-	Get(tableName string) error
+	GetLastQuery() string
+	GetResults() []map[string]string
+	GetSingleResult() map[string]string
+
+	Select(col ...string) *RormEngine
+	SelectSum(col string) *RormEngine
+	SelectAverage(col string) *RormEngine
+
+	Where(col, value string, opt ...string) *RormEngine
+	WhereIn(col string, listOfValues ...interface{}) *RormEngine
+	WhereNotIn(col string, listOfValues ...interface{}) *RormEngine
+	WhereLike(col, value string) *RormEngine
+
+	Or(col, value string, opt ...string) *RormEngine
+	OrIn(col string, listOfValues ...interface{}) *RormEngine
+	OrNotIn(col string, listOfValues ...interface{}) *RormEngine
+	OrLike(col, value string) *RormEngine
+
+	OrderBy(col, value string) *RormEngine
+	Limit(limit int, offset ...int) *RormEngine
+	From(tableName string) *RormEngine
+
+	Get(pointerStruct interface{}) error
+
+	Insert(data interface{}) error
+	Update(data interface{}) error
 }

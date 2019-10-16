e6129f95c10afb1dea9a9b165e32a1d7fa10071b
Raditya Pratama <radityaapratamaa@gmail.com>
2019-10-16 08:54:08 +0700

    register new function

--- interface.go
+++ interface.go
@@ -2,9 +2,7 @@ package rorm
 
 import "database/sql"
 
-// import rormEngine "github.com/radityaapratamaa/rorm/engine"
-
-type EngineInterface interface {
+type Rorm interface {
 	SetDB(db *sql.DB)
 	GetDB() *sql.DB
 	GetPreparedValues() []interface{}
@@ -15,31 +13,31 @@ type EngineInterface interface {
 	GetResults() []map[string]string
 	GetSingleResult() map[string]string
 
-	Select(col ...string) *RormEngine
-	SelectSum(col string, colAlias ...string) *RormEngine
-	SelectAverage(col string, colAlias ...string) *RormEngine
-	SelectMax(col string, colAlias ...string) *RormEngine
-	SelectMin(col string, colAlias ...string) *RormEngine
-	SelectCount(col string, colAlias ...string) *RormEngine
+	Select(col ...string) *Engine
+	SelectSum(col string, colAlias ...string) *Engine
+	SelectAverage(col string, colAlias ...string) *Engine
+	SelectMax(col string, colAlias ...string) *Engine
+	SelectMin(col string, colAlias ...string) *Engine
+	SelectCount(col string, colAlias ...string) *Engine
 
-	Where(col, value string, opt ...string) *RormEngine
-	WhereIn(col string, listOfValues ...interface{}) *RormEngine
-	WhereNotIn(col string, listOfValues ...interface{}) *RormEngine
-	WhereLike(col, value string) *RormEngine
+	Where(col, value string, opt ...string) *Engine
+	WhereIn(col string, listOfValues ...interface{}) *Engine
+	WhereNotIn(col string, listOfValues ...interface{}) *Engine
+	WhereLike(col, value string) *Engine
 
-	Or(col, value string, opt ...string) *RormEngine
-	OrIn(col string, listOfValues ...interface{}) *RormEngine
-	OrNotIn(col string, listOfValues ...interface{}) *RormEngine
-	OrLike(col, value string) *RormEngine
+	Or(col, value string, opt ...string) *Engine
+	OrIn(col string, listOfValues ...interface{}) *Engine
+	OrNotIn(col string, listOfValues ...interface{}) *Engine
+	OrLike(col, value string) *Engine
 
-	OrderBy(col, value string) *RormEngine
-	Asc(col string) *RormEngine
-	Desc(col string) *RormEngine
+	OrderBy(col, value string) *Engine
+	Asc(col string) *Engine
+	Desc(col string) *Engine
 
-	Limit(limit int, offset ...int) *RormEngine
-	From(tableName string) *RormEngine
+	Limit(limit int, offset ...int) *Engine
+	From(tableName string) *Engine
 
-	Raw(rawQuery string) *RormEngine
+	Raw(rawQuery string) *Engine
 	Get(pointerStruct interface{}) error
 
 	Insert(data interface{}) error
